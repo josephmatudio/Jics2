9df2aa079e4347db37fd435519ee022f6bfe28d3
josephmatudio <joseph.matudio.cvsu.edu.ph>
2021-05-05 14:26:39 +0800

    second backup

---
+++ wp-content/themes/blank/home-template.php
@@ -0,0 +1,67 @@
+<?php
+
+/* Template Name: HOME */
+get_header();
+?>
+	 	<p class="lead text-center">
+	 		<br>
+            <br>
+            ARTHUR S. JORDA<br><strong>
+            Let's Go to JICS Computer Tradings and Build your dream Gaming System.
+            </strong>
+            <br>
+            <br>
+        </p>
+	<div class="container">
+    <!-- slideshow-->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="http://localhost/wordpress/wp-content/uploads/2021/05/download-1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="http://localhost/wordpress/wp-content/uploads/2021/05/images.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="http://localhost/wordpress/wp-content/uploads/2021/05/download-2.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <br>
+    <br>
+    <footer class="container-fluid text-center">
+    <div class="row">
+      <div class="col-sm-4">
+          <br>
+          <h4>Sample Product</h4>
+          <img src="http://localhost/wordpress/wp-content/uploads/2021/04/Monitor3.jpg">
+          <h2><strong>27" NVision Curved Monitor</strong></h2>
+      </div>
+      <div class="col-sm-4">
+          <br>
+          <h4>Sample Product</h4>
+          <img src="http://localhost/wordpress/wp-content/uploads/2021/04/Monitor3.jpg">
+        <h2><strong>27" NVision Curved Monitor</strong></h2>
+    </div>
+      <div class="col-sm-4">
+          <br>
+          <h4>Sample Product</h4>
+          <img src="http://localhost/wordpress/wp-content/uploads/2021/04/Monitor3.jpg">
+        <h2><strong>27" NVision Curved Monitor</strong></h2>
+    </div>
+    </div>
+</footer>
+<?php get_footer();?>
